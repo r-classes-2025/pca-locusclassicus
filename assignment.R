a09493cd06fd526dcb3c511d7f3af5445c477d0f
locusclassicus <86204710+locusclassicus@users.noreply.github.com>
2026-01-30 19:31:18 +0300

    test Bogdanoviсh code

--- assignment.R
+++ assignment.R
@@ -1,24 +1,24 @@
 # установите и загрузите пакеты
 library(friends)
-library(tidyverse) 
+library(tidyverse)
 library(tidytext)
-library(factoextra)
+library(factoextra) 
 
 # 1. отберите 6 главных персонажей (по количеству реплик)
 # сохраните как символьный вектор
-top_speakers <- count(friends, speaker, sort = TRUE) |> 
-  slice_head(n = 6) |> 
+top_speakers <- friends |> 
+  count(speaker, sort = TRUE) |>
+  head(6) |>
   pull(speaker)
 
-# 2. отфильтруйте топ-спикеров,
+# 2. отфильтруйте топ-спикеров, 
 # токенизируйте их реплики, удалите из них цифры
 # столбец с токенами должен называться word
 # оставьте только столбцы speaker, word
-friends_tokens <- friends |>
+friends_tokens <- friends |> 
   filter(speaker %in% top_speakers) |>
   unnest_tokens(word, text) |>
-  mutate(word = str_remove_all(word, "\\d+")) |>
-  filter(word != "") |>
+  filter(!str_detect(word, "\\d")) |>
   select(speaker, word)
 
 # 3. отберите по 500 самых частотных слов для каждого персонажа
@@ -26,15 +26,14 @@ friends_tokens <- friends |>
 friends_tf <- friends_tokens |>
   count(speaker, word) |>
   group_by(speaker) |>
+  slice_max(n, n = 500, with_ties = FALSE) |>
   mutate(tf = n / sum(n)) |>
-  arrange(-n) |> 
-  slice_head(n = 500) |>
   ungroup() |>
   select(speaker, word, tf)
 
-# 4. преобразуйте в широкий формат;
-# столбец c именем спикера превратите в имя ряда, используя подходящую функцию
-friends_tf_wide <- friends_tf |>
+# 4. преобразуйте в широкий формат; 
+# столбец c именем спикера превратите в имя ряда, используя подходящую функцию 
+friends_tf_wide <- friends_tf |> 
   pivot_wider(names_from = word, values_from = tf, values_fill = 0) |>
   column_to_rownames("speaker")
 
@@ -46,20 +45,4 @@ km.out <- kmeans(scale(friends_tf_wide), centers = 3, nstart = 20)
 
 # 6. примените к матрице метод главных компонент (prcomp)
 # центрируйте и стандартизируйте, использовав аргументы функции
-pca_fit <- prcomp(friends_tf_wide, center = TRUE, scale. = TRUE)
-
-# 7. Покажите наблюдения и переменные вместе (биплот)
-# в качестве геома используйте текст (=имя персонажа)
-# цветом закодируйте кластер, выделенный при помощи k-means
-# отберите 20 наиболее значимых переменных (по косинусу, см. документацию к функции)
-# сохраните график как переменную q
-q <- fviz_pca_biplot(
-  pca_fit,
-  geom = c("text"),
-  habillage = as.factor(km.out$cluster),
-  select.var = list(cos2 = 20),
-  repel = FALSE,
-  ggtheme = theme_minimal()
-) +
-  theme(legend.position = "none")
-
+pca_fit <- prcomp(friends_tf_wide, scale = TRUE, center = TRUE)
